97252c0d4a46a8f8888f6c85dcd99d336414e7b4
Bulzart <86388058+bulzart@users.noreply.github.com>
2022-03-14 16:56:01 +0100

    14/03/2022 mid3

--- app/Http/Controllers/FamilyPersonsController.php
+++ app/Http/Controllers/FamilyPersonsController.php
@@ -20,57 +20,45 @@ class FamilyPersonsController extends Controller
 {
     public function family_persons($id,$admin_id = null,Request $req)
     {
-
+ 
      Session::put('pend_id',(int) $req->pend_id);
-
+ 
         $idd = Crypt::decrypt($id);
         $idd /= 1244;
         $cnt = 0;
         $cnt1 = 0;
         $lead = family::find($idd);
-
-
-        $data = LeadDataKK::where('person_id', '=', $idd)->first();
+ $admin_id = $req->admin_id;
+ 
 
 
             if (Auth::guard('admins')->user()->hasRole('fs')) {
-
-                if ($lead->lead->assign_to_id == Auth::user()->id || Pendency::find((int) $req->pend_id)->admin_id == Auth::user()->id) {
+                if (Auth::guard('admins')->user()->id == $lead->lead->assign_to_id || Pendency::find((int) $req->pend_id)->admin_id == Auth::user()->id) {
                     try {
-
-                   
+                        
+                        $data = LeadDataKK::where('person_id', '=', $idd)->firstOrFail();
+                       
                         return redirect()->route('acceptdata', [Crypt::encrypt($idd*1244),'accept' => false,'admin_id' => $admin_id]);
                     }
                     catch (Exception $e) {
-
+                      
                         return view('documentsform', compact('lead'));
                     }
                 }
                 else {
-
+               
                     return redirect()->back();
                 }
             }
-
             else {
-               
-
                 try {
-
-                    if($data->imported == 0){
+                    $data = LeadDataKK::where('person_id', '=', $idd)->firstOrFail();
                     return redirect()->route('acceptdata', [Crypt::encrypt($idd*1244),'accept' => false,'admin_id' => $admin_id]);
-                    }
-                    else{
-                        return view('documentsform', compact('lead'));
-                    }
                 }
                 catch (Exception $e) {
                     return view('documentsform', compact('lead'));
                 }
             }
-
-
-
     }
 
 
