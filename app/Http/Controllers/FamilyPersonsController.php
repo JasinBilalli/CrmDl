3ffd79c99a72889ccc635d82a59493bb7f5abe77
Bulzart <86388058+bulzart@users.noreply.github.com>
2022-03-14 17:04:43 +0100

    Update FamilyPersonsController.php

--- app/Http/Controllers/FamilyPersonsController.php
+++ app/Http/Controllers/FamilyPersonsController.php
@@ -1,4 +1,24 @@
-public function family_persons($id,$admin_id = null,Request $req)
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\family;
+use App\Models\FamilyPerson;
+use App\Models\lead;
+use App\Models\data;
+use App\Models\LeadDataKK;
+use App\Models\Pendency;
+use Exception;
+use Illuminate\Support\Facades\Session;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Throwable;
+use Illuminate\Support\Facades\Crypt;
+
+
+class FamilyPersonsController extends Controller
+{
+    public function family_persons($id,$admin_id = null,Request $req)
     {
  
      Session::put('pend_id',(int) $req->pend_id);
@@ -40,3 +60,40 @@ public function family_persons($id,$admin_id = null,Request $req)
                 }
             }
     }
+
+
+    public function getAllFamilyPersonsOfLead($id)
+    {
+        $familyPersons = family::where('leads_id', $id)->get();
+        return $familyPersons;
+    }
+
+    public function updateFamilyPerson($id, Request $request)
+    {
+        $family =  family::where('id', $id)->get();
+        if (Auth::guard('admins')->user()->hasRole('admin') || Auth::guard('admins')->user()->hasRole('backoffice') ||  $family->lead->assign_to_id == Auth::guard('admins')->user()->id) {
+            $family->update($request->all());
+            return redirect()->back()->with('message', 'Familienmitglied wurde aktualisiert');
+        } else {
+            return redirect()->back();
+        }
+    }
+
+    public function deleteFamilyPerson($id, $leadId)
+    {
+        $family = family::where('id', $id)->where('leads_id', $leadId)->get();
+        if (Auth::guard('admins')->user()->hasRole('admin') || Auth::guard('admins')->user()->hasRole('backoffice') || Auth::guard('admins')->user()->id == $family->lead->id) {
+            $family->delete();
+        }
+    }
+
+    public function updateleadfamilyperson(Request $request, $id)
+    {
+
+        $idd = Crypt::decrypt($id);
+        $idd /= 1244;
+
+        family::where('id', $idd)->update(['first_name' => $request->familyfirstname, 'last_name' => $request->familylastname]);
+        return redirect()->back()->with('success', 'Update erfolgreich');
+    }
+}
